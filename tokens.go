e23b37f4609880ef3c0e35ec009e12d32f05b1d6
jekuz <jekuz@mail.ru>
2021-01-19 20:49:19 +0100

    fixed typos

--- tokens.go
+++ tokens.go
@@ -18,14 +18,18 @@ var secretATkey, secretRTkey string = "secretATkey", "secretRTkey"
 // createAT
 func createAT(guid string) string{
 	return calcJWT(jwt.NewWithClaims(jwt.SigningMethodHS512,
-		jwt.MapClaims{"data": guid,
-		              "exp" : time.Now().Add(time.Minute * 15).Unix()}), secretATkey)
+		jwt.MapClaims{
+			"data": guid,
+			"exp" : time.Now().Add(time.Minute * 15).Unix(),
+			}), secretATkey)
 }
 // createRT
 func createRT(AT string) string{
 	return calcJWT(jwt.NewWithClaims(jwt.SigningMethodHS512,
-			jwt.MapClaims{"data": strings.Split(AT,".")[2][:10],
-						  "exp" : time.Now().Add(time.Hour * 24).Unix()}), secretRTkey)
+			jwt.MapClaims{
+				"data": strings.Split(AT,".")[2][:10],
+				"exp" : time.Now().Add(time.Hour * 24).Unix(),
+				}), secretRTkey)
 }
 // calcJWT
 func calcJWT(headAndPayload *jwt.Token, key string) string{
@@ -37,10 +41,12 @@ func getATguid(AT string) string{
 	if isJWT(AT){
 		var data data = getJWTpayload(AT)
 		if AT == calcJWT(jwt.NewWithClaims(jwt.SigningMethodHS512,
-				jwt.MapClaims{"data": data.Data,
-							  "exp" : data.Exp}), secretATkey){
-								  return data.Data
-								}
+				jwt.MapClaims{
+					"data": data.Data,
+					"exp" : data.Exp,
+					}), secretATkey){
+						return data.Data
+					}
 		}
 	return ""
 }
@@ -50,8 +56,9 @@ func isRTvalid(AT, RT string) bool{
 		var data data = getJWTpayload(RT)
 		if data.Exp > int(time.Now().Unix()){
 			if RT == calcJWT(jwt.NewWithClaims(jwt.SigningMethodHS512,
-				jwt.MapClaims{"data": strings.Split(AT,".")[2][:10],
-							  "exp" : data.Exp}), secretRTkey){
+				jwt.MapClaims{
+					"data": strings.Split(AT,".")[2][:10],
+					"exp" : data.Exp}), secretRTkey){
 								  return true
 								}
 		}	
